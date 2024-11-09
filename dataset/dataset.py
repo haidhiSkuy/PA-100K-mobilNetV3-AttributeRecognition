2808b88cfa8dc1e213b3dbf8b92fd799ec56eead
Haidhi Angkawijana <haidhiangkawijana@gmail.com>
2024-11-10 00:40:26 +0700

    add torch dataset

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,6 +1,8 @@
 import torch
+import lightning as L
 from PIL import Image
-from torch.utils.data import Dataset
+from torchvision.transforms import v2
+from torch.utils.data import Dataset, DataLoader
 
 class PersonAppereanceDataset(Dataset):
     def __init__(self, image_list, label_list, transform):
@@ -21,4 +23,40 @@ class PersonAppereanceDataset(Dataset):
         if self.transform: 
             transformed = self.transform(image)
 
-        return transformed, label_tensor.to(torch.float32)
+        return transformed, label_tensor.to(torch.float32)  
+    
+
+class PersonAppereanceDataModule(L.LightningDataModule):
+    def __init__(
+            self,
+            train_images : list[str], 
+            train_labels : list[int], 
+            valid_images : list[str], 
+            valid_labels : list[int],
+            batch_size : int = 32
+        ) -> None: 
+        super().__init__()
+
+        self.train_images = train_images 
+        self.train_labels = train_labels 
+        self.valid_images = valid_images 
+        self.valid_labels = valid_labels
+        self.batch_size = batch_size
+
+    def get_transform(self):
+        transform = v2.Compose([
+            v2.Resize((128, 256)),
+            v2.ToTensor(),
+        ]) 
+        return transform  
+    
+    def setup(self, stage: str):
+        transform = self.get_transform()
+        self.train_dataset = PersonAppereanceDataset(self.train_images, self.train_labels, transform) 
+        self.valid_dataset = PersonAppereanceDataset(self.valid_images, self.valid_labels, transform) 
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.batch_size)
+    
+    def val_dataloader(self):
+        return DataLoader(self.valid_dataset, batch_size=self.batch_size)
