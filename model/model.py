cc278ab5a70082ef37b5663d3131ed331f268284
Haidhi Angkawijana <haidhiangkawijana@gmail.com>
2024-11-09 19:50:31 +0700

    rename

--- model/model.py
+++
@@ -1,8 +0,0 @@
-import torch 
-from torch import nn
-from torchvision.models import mobilenet_v3_small 
-
-NUM_CLASSES = 26
-
-model = mobilenet_v3_small() 
-model.classifier[3] = nn.Linear(in_features=1024, out_features=NUM_CLASSES) 
